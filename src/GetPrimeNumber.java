42471319bed383036ee8e29c762f848be8558dd7
nunbitromance <sunghyolee@hotmail.com>
2015-03-22 16:23:20 -0700

    Update GetPrimeNumber.java

--- src/GetPrimeNumber.java
+++ src/GetPrimeNumber.java
@@ -2,15 +2,20 @@ find the next prime number after n
 
 public int getPrime(int n) {
   int target = n;
-  while (true) {
+  boolean foundPrime = false;
+  while (!foundPrime) {
     target = target + 1;
     int sqrt = Math.sqrt(target);
+    boolean quit = false;
     for (int i = 2; i < sqrt; i++) {
       if (target % i == 0) {
+        quit = true;
         break;
       }
     }
-    return target;
+    if (!quit) {
+      foundPrime = true;
+    }
   }
   return target;
 }
