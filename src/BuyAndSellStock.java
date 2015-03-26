527b4881790ef23d58f219426cd016e1cbaf9dc3
nunbitromance <sunghyolee@hotmail.com>
2015-03-26 07:20:53 -0700

    Update BuyAndSellStock.java

--- src/BuyAndSellStock.java
+++ src/BuyAndSellStock.java
@@ -1,3 +1,9 @@
+/*
+Best Time to Buy and Sell Stock Total Accepted: 44390 Total Submissions: 137184 My Submissions Question Solution 
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+*/
 public int maxProfix(int[] prices) {
   int minIndex = 0;
   int maxDiff = 0;
