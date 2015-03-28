9c22a0b53a59486ebe0417bd4faf843dfc8564e3
nunbitromance <sunghyolee@hotmail.com>
2015-03-28 14:31:05 -0700

    Create UberInterviewQuestions.java

---
+++ src/UberInterviewQuestions.java
@@ -0,0 +1,17 @@
+Phone screen: Fill in this function header, which is   responsible for finding all vehicle stops closest to a user's GPS location. 
+
+On-Site: A design question (How would you design a ride-share application?) A technical communication question. A fairly simple coding question involving some OOP. A design discussion with the hiring manager. 
+
+Using Object Oriented Design principles, design a method to check if a Sudoku board is valid (skeleton code was provided which was initially passed in through a 2-d array).
+
+Implement boggle
+
+Design a price surge system, both at a high level and the architecture
+
+All the questions were open ended like how would you design netflix,online spreadsheet, tell me about a challenging situation/issue in your current/past project etc 
+
+Implement LRU cache with get and set operations in constant time O(1). 
+
+Given a regular expression pattern and a string, check to see if pattern matches the string. 
+
+Design a distributed system for sorting of large files.
