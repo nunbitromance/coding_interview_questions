766353ddc24658fe099eadf3c8c1492f7523e2b6
nunbitromance <sunghyolee@hotmail.com>
2015-03-29 15:34:54 -0700

    Create Singleton.java

---
+++ src/Singleton.java
@@ -0,0 +1,32 @@
+//Lazy instantiation using double locking mechanism.
+class Singleton
+{
+	private static Singleton instance;
+
+	private Singleton()
+	{
+	System.out.println("Singleton(): Initializing Instance");
+	}
+
+	public static Singleton getInstance()
+	{
+		if (instance == null)
+		{
+			synchronized(Singleton.class)
+			{
+				if (instance == null)
+				{
+					System.out.println("getInstance(): First time getInstance was invoked!");
+					instance = new Singleton();
+				}
+			}            
+		}
+
+		return instance;
+	}
+
+	public void doSomething()
+	{
+		System.out.println("doSomething(): Singleton does something!");
+	}
+}
