b6ddc3f3f2e3eb610ab298d607963fd530b6bc5f
nunbitromance <sunghyolee@hotmail.com>
2015-04-15 00:35:32 -0700

    Update CountTrees.java

--- src/CountTrees.java
+++ src/CountTrees.java
@@ -13,21 +13,22 @@ Given n = 3, there are a total of 5 unique BST's.
    2     1         2                 3
 */
 
-public static int CountTrees(int n)
-{
-	if (n == 0 || n == 1)
-	{
-		return n;
-	}
-	
-	int sum = 0; int left, right = 0;
-	for (int k = 0; k < n; k++)
-	{
-		left = CountTrees(k);
-		right = CountTrees(n - k - 1);
-		
-		sum += left * right;
-	}
-	
-	return count;
-}
+    public int numTrees(int n) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        return numTrees(n, map);
+    }
+    
+    private int numTrees(int n, Map<Integer, Integer> map) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += numTrees(i - 1, map) * numTrees(n - i, map);
+        }
+        map.put(n, result);
+        return result;
+    }
