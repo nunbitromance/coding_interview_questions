a23b76bf5494760a82774fab10c1212d8e439887
nunbitromance <sunghyolee@hotmail.com>
2015-04-20 08:13:52 -0700

    Create GetAllPrimeNumbers.java

---
+++ src/GetAllPrimeNumbers.java
@@ -0,0 +1,15 @@
+public boolean[] sieve(int n)
+{
+   boolean[] prime=new boolean[n+1];
+   Arrays.fill(prime,true);
+   prime[0]=false;
+   prime[1]=false;
+   int m=Math.sqrt(n);
+
+   for (int i=2; i<=m; i++)
+      if (prime[i])
+         for (int k=i*i; k<=n; k+=i)
+            prime[k]=false;
+
+   return prime;
+} 
