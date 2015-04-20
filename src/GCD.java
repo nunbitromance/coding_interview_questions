3176cefef7b4a0bf1641ad5c4e4c432cd2a643a3
nunbitromance <sunghyolee@hotmail.com>
2015-04-20 08:15:59 -0700

    Create GCD.java

---
+++ src/GCD.java
@@ -0,0 +1,14 @@
+
+public int getGcd(int a, int b) {
+  for (int i=Math.min(a,b); i>=1; i--)
+  if (a%i==0 && b%i==0)
+    return i;
+  }
+}
+
+//assume that a and b cannot both be 0
+public int GCD(int a, int b)
+{
+   if (b==0) return a;
+   return GCD(b,a%b);
+}
