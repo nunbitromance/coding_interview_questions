050ef60ce67da5e7f42e2616a9d4de18840af45a
nunbitromance <sunghyolee@hotmail.com>
2015-04-21 01:14:00 -0700

    Update GetPrimeNumber.java

--- src/GetPrimeNumber.java
+++ src/GetPrimeNumber.java
@@ -19,3 +19,12 @@ public int getPrime(int n) {
   }
   return target;
 }
+
+//checks whether an int is prime or not.
+boolean isPrime(int n) {
+    for(int i=2;2*i<n;i++) {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
