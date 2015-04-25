145f55a27e88ae8484e2bfc8b5f26ec89047281c
nunbitromance <sunghyolee@hotmail.com>
2015-04-24 21:27:40 -0700

    Update Combinations.java

--- src/Combinations.java
+++ src/Combinations.java
@@ -50,3 +50,29 @@ C(4, 2)
 3, 4
 
 4
+
+public ArrayList<ArrayList<Integer>> combine(int n, int k) {
+	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+ 
+	if (n <= 0 || n < k)
+		return result;
+ 
+	ArrayList<Integer> item = new ArrayList<Integer>();
+	dfs(n, k, 1, item, result); // because it need to begin from 1
+ 
+	return result;
+}
+ 
+private void dfs(int n, int k, int start, ArrayList<Integer> item,
+		ArrayList<ArrayList<Integer>> res) {
+	if (item.size() == k) {
+		res.add(new ArrayList<Integer>(item));
+		return;
+	}
+ 
+	for (int i = start; i <= n; i++) {
+		item.add(i);
+		dfs(n, k, i + 1, item, res);
+		item.remove(item.size() - 1);
+	}
+}
