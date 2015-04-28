f563605a4c21edefe9b7bed90333df9ddc149323
nunbitromance <sunghyolee@hotmail.com>
2015-04-28 10:06:49 -0700

    Update AddParenthesis.java

--- src/AddParenthesis.java
+++ src/AddParenthesis.java
@@ -1,60 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ConsoleApplication3
-{
-    public class Q9_6_AddParenthesis
-    {
-       /* private static void Main(string[] args)
-        {
-            List<string> list = new List<string>();
-            AddParen(list, 3, 3, new char[6], 0);
-            Console.ReadLine();
-        }*/
-
-        public static void AddParen(List<string> list, int lCount, int rCount, char[] s, int index)
-        {
-            if (lCount == 0 && rCount == 0)
-            {
-                string str = new string(s);
-                list.Add(str);
-            }
-
-            if (lCount > 0)
-            {
-                s[index] = '(';
-                AddParen(list, lCount - 1, rCount, s, index + 1);
-            }
-
-            if (rCount > lCount)
-            {
-                s[index] = ')';
-                AddParen(list, lCount, rCount - 1, s, index + 1);
-            }
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<String>();
+        helper(0, 0, n, "", result);
+        return result;
+    }
+    
+    private void helper(int l, int r, int n, String s, List<String> result) {
+        if (l == n && r == n) {
+            result.add(s);
+            return;
         }
-
-        // 2nd implementation: print all possible combinations of parenthesis
-        // ()()(), (())(), ((())), (()())
-        public static void getParenthesis(char[] p, int lCount, int rCount, int index)
-        {
-        	if (lCount == 0 && rCount == 0)
-        	{
-        		print(p);
-        	}
-        	if (lCount > 0)
-        	{
-        		getParenthesis(p, lCount - 1, rCount, index + 1);
-        	}
-        	if (rCount > lCount)
-        	{
-        		getParenthesis(p, lCount, rCount - 1, index + 1);
-        	}
+        
+        if (l < n) {
+            helper(l+1, r, n, s + "(", result);
+        }
+        
+        if (l > r) {
+            helper(l, r+1, n, s + ")", result);
         }
     }
 }
-
-
-
-
