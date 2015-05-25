21dc94016e38e6d19d1dbe18b7a6e7f4b0b5cc74
nunbitromance <sunghyolee@hotmail.com>
2015-05-25 12:50:34 -0700

    Update BreadthFirstSearch.java

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -8,22 +8,23 @@
   Visit order: 3 -> 9 -> 20 -> 15 -> 7
 */
 // Non-recursion method
-public static void BFS(Node root)
+public static Node bfs(Node root, int target)
 {
-	Queue<Node> q = new Queue<Node>();
-	q.Enqueue(root);
-	
-	while (q.IsEmpty() == false)
+	Queue<Node> queue = new LinkedList<Node>();
+	queue.offer(root);
+	Set<Node> visited = new HashSet<Node>();
+	while (!queue.isEmpty())
 	{
-		Node cur = q.Dequeue();
-		cur.Visited = true;
-		foreach (Node c in cur.Children)
-		{
-			if (c.Visited == false)
-			{
-				c.Visited = true;
-				q.Enqueue(c);	
+		Node cur = queue.poll();
+		if (cur.value == target) {
+			return cur;
+		}
+		visited.add(cur);
+		for (Node c : cur.children) {
+			if (!visited.contains(c)) {
+				queue.offer(c);	
 			}
 		}
 	}
+	return null;
 }
