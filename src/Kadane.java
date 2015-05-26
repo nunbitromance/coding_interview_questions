58f7f0cce4f3591373b80faf4b6630d1d57d33ed
nunbitromance <sunghyolee@hotmail.com>
2015-05-26 09:39:55 -0700

    Create Kadane.java

---
+++ src/Kadane.java
@@ -0,0 +1,20 @@
+private KadaneResult kadane(int arr[]){
+        int max = 0;
+        int maxStart = -1;
+        int maxEnd = -1;
+        int currentStart = 0;
+        int maxSoFar = 0;
+        for(int i=0; i < arr.length; i++){
+            maxSoFar += arr[i];
+            if(maxSoFar < 0){
+                maxSoFar = 0;
+                currentStart = i+1;
+            }
+            if(max < maxSoFar){
+                maxStart = currentStart;
+                maxEnd = i;
+                max = maxSoFar;
+            }
+        }
+        return new KadaneResult(maxSoFar, maxStart, maxEnd);
+    }
