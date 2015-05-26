fb59efaa17f5fbfa209c55bf1d913bb51f9684bb
nunbitromance <sunghyolee@hotmail.com>
2015-05-26 08:38:55 -0700

    Update ReverseWords.java

--- src/ReverseWords.java
+++ src/ReverseWords.java
@@ -16,28 +16,27 @@ class Solution {
 	}
 }
 
-private static void reverse(char[] buf, int start, int end) {
-    for (int i = start, j = end - 1; i < j; i++, j--) {
-        char swap = buf[i];
-        buf[i] = buf[j];
-        buf[j] = swap;
+/*Function to reverse words*/
+void reverseWords(char *s)
+{
+  char *word_begin = s;
+  char *temp = s; /* temp is for word boundry */
+ 
+  /*STEP 1 of the above algorithm */
+  while( *temp )
+  {
+    temp++;
+    if (*temp == '\0')
+    {
+      reverse(word_begin, temp-1);
     }
-}
-
-public static String reverseWords1(String sentence) {
-    char[] buf = sentence.toCharArray();
-
-    // Reverse the string, character-wise
-    reverse(buf, 0, buf.length);
-
-    // Within each word, reverse the characters again
-    int wordEnd = 0;
-    for (int wordStart = 0; wordStart < buf.length; wordStart = wordEnd + 1) {
-        for (wordEnd = wordStart; wordEnd < buf.length && buf[wordEnd] != ' '; wordEnd++) {}
-
-        // wordStart is at the start of a word.
-        // wordEnd is just past the end of the word.
-        reverse(buf, wordStart, wordEnd);
+    else if(*temp == ' ')
+    {
+      reverse(word_begin, temp-1);
+      word_begin = temp+1;
     }
-    return new String(buf);
+  } /* End of while */
+ 
+   /*STEP 2 of the above algorithm */
+  reverse(s, temp-1);
 }
