1f988a6f1d435edc7dad6a612cf78e88bd2fcbd5
nunbitromance <sunghyolee@hotmail.com>
2015-05-28 00:11:35 -0700

    Update MaxWindow.java

--- src/MaxWindow.java
+++ src/MaxWindow.java
@@ -58,3 +58,33 @@ public static int[] MaxWindow(int[] a, int w)
 	
 	return b;
 }
+
+/*
+http://articles.leetcode.com/2011/01/sliding-window-maximum.html
+You might be wondering: Is there a better way of doing this without using a heap? How about using a double-ended queue? (A linked list should be fine too)
+
+The double-ended queue is the perfect data structure for this problem. It supports insertion/deletion from the front and back. The trick is to find a way such that the largest element in the window would always appear in the front of the queue. How would you maintain this requirement as you push and pop elements in and out of the queue?
+
+Besides, you might notice that there are some redundant elements in the queue that we shouldn’t even consider about. For example, if the current queue has the elements: [10 5 3], and a new element in the window has the element 11. Now, we could have emptied the queue without considering elements 10, 5, and 3, and insert only element 11 into the queue.
+
+A natural way most people would think is to try to maintain the queue size the same as the window’s size. Try to break away from this thought and try to think outside of the box. Removing redundant elements and storing only elements that need to be considered in the queue is the key to achieve the efficient O(n) solution below.
+
+*/
+
+void maxSlidingWindow(int A[], int n, int w, int B[]) {
+  deque<int> Q;
+  for (int i = 0; i < w; i++) {
+    while (!Q.empty() && A[i] >= A[Q.back()])
+      Q.pop_back();
+    Q.push_back(i);
+  }
+  for (int i = w; i < n; i++) {
+    B[i-w] = A[Q.front()];
+    while (!Q.empty() && A[i] >= A[Q.back()])
+      Q.pop_back();
+    while (!Q.empty() && Q.front() <= i-w)
+      Q.pop_front();
+    Q.push_back(i);
+  }
+  B[n-w] = A[Q.front()];
+}
