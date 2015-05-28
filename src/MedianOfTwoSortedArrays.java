d249edf8a0e4ca2e128af0bbfac0463dc05cfb8f
nunbitromance <sunghyolee@hotmail.com>
2015-05-28 13:07:05 -0700

    Create MedianOfTwoSortedArrays.java

---
+++ src/MedianOfTwoSortedArrays.java
@@ -0,0 +1,96 @@
+/* This function returns median of ar1[] and ar2[].
+   Assumptions in this function:
+   Both ar1[] and ar2[] are sorted arrays
+   Both have n elements */
+int getMedian(int ar1[], int ar2[], int n)
+{
+    int m1; /* For median of ar1 */
+    int m2; /* For median of ar2 */
+ 
+    /* return -1  for invalid input */
+    if (n <= 0)
+        return -1;
+ 
+    if (n == 1)
+        return (ar1[0] + ar2[0])/2;
+ 
+    if (n == 2)
+        return (max(ar1[0], ar2[0]) + min(ar1[1], ar2[1])) / 2;
+ 
+    m1 = median(ar1, n); /* get the median of the first array */
+    m2 = median(ar2, n); /* get the median of the second array */
+ 
+    /* If medians are equal then return either m1 or m2 */
+    if (m1 == m2)
+        return m1;
+ 
+     /* if m1 < m2 then median must exist in ar1[m1....] and ar2[....m2] */
+    if (m1 < m2)
+    {
+        if (n % 2 == 0)
+            return getMedian(ar1 + n/2 - 1, ar2, n - n/2 +1);
+        else
+            return getMedian(ar1 + n/2, ar2, n - n/2);
+    }
+ 
+    /* if m1 > m2 then median must exist in ar1[....m1] and ar2[m2...] */
+    else
+    {
+        if (n % 2 == 0)
+            return getMedian(ar2 + n/2 - 1, ar1, n - n/2 + 1);
+        else
+            return getMedian(ar2 + n/2, ar1, n - n/2);
+    }
+}
+
+
+* This function returns median of ar1[] and ar2[].
+   Assumptions in this function:
+   Both ar1[] and ar2[] are sorted arrays
+   Both have n elements */
+int getMedian(int ar1[], int ar2[], int n)
+{
+    int i = 0;  /* Current index of i/p array ar1[] */
+    int j = 0; /* Current index of i/p array ar2[] */
+    int count;
+    int m1 = -1, m2 = -1;
+ 
+    /* Since there are 2n elements, median will be average
+     of elements at index n-1 and n in the array obtained after
+     merging ar1 and ar2 */
+    for (count = 0; count <= n; count++)
+    {
+        /*Below is to handle case where all elements of ar1[] are
+          smaller than smallest(or first) element of ar2[]*/
+        if (i == n)
+        {
+            m1 = m2;
+            m2 = ar2[0];
+            break;
+        }
+ 
+        /*Below is to handle case where all elements of ar2[] are
+          smaller than smallest(or first) element of ar1[]*/
+        else if (j == n)
+        {
+            m1 = m2;
+            m2 = ar1[0];
+            break;
+        }
+ 
+        if (ar1[i] < ar2[j])
+        {
+            m1 = m2;  /* Store the prev median */
+            m2 = ar1[i];
+            i++;
+        }
+        else
+        {
+            m1 = m2;  /* Store the prev median */
+            m2 = ar2[j];
+            j++;
+        }
+    }
+ 
+    return (m1 + m2)/2;
+}
