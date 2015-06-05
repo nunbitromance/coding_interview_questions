d245916ab53a46b3ccfdc3625c21aaa2506b8c88
nunbitromance <sunghyolee@hotmail.com>
2015-06-05 08:08:59 -0700

    Update CelebrityProblem.java

--- src/CelebrityProblem.java
+++ src/CelebrityProblem.java
@@ -23,3 +23,63 @@ public boolean isInfluencerExists(int[][] graph) {
 	}
 	return false;
 }
+
+/*
+                A
+             ^  ^  ^
+            /   |   \ 
+           /    |    \
+          B <-> C     \
+                 ^     \
+                  \     \
+                   ------D
+
+B knows A and C
+C knows A and B
+D knows A and C                   
+*/
+public class Person {
+    public boolean knows(Person n);
+}                   
+
+1. pick A as candidate
+2. is B pointing to A? yes, rule out B
+3. is C pointing to A? yes, rule out C
+4. is D pointing to A? yes, rule out D
+5. does A know any one of {B, C, D}?
+
+D,B,C,A
+1. pick D as candidate
+2. is B pointing to D? no, rule out D
+3. pick B as candidate
+4. is C pointing to B? yes, rule out C
+5. is A pointing to B? no, pick A as candidate
+6. does {B, C, D} know A && A does not know anyone else?
+
+// Return the lonely celebrity (if one exists)
+Person findLonelyCelebrity(List<Person> people) {
+    // validation
+    if (people == null) {
+        throw new IllegalArgException("people is null");
+    } else if (people.size() == 0) {
+        return null;
+    }
+
+    Person candidate = people.get(0);
+    for (int i = 1; i < people.size(); i++) {
+        boolean knows = people.get(i).knows(candidate);
+        if (!knows) {
+            candidate = people.get(i);
+        }
+    }
+    
+    // check whether candidate is real
+    for (int i = 0; i < people.size(); i++) {
+        if (!candidate.equals(people.get(i)) && (!people.get(i).knows(candidate) || candidate.knows(people.get(i)))) {
+            candidate = null;
+            break;
+        }
+    }
+
+    return candidate;
+}
