3939ff4f9f913193222925818c512588b67b653e
nunbitromance <sunghyolee@hotmail.com>
2015-06-28 16:00:29 -0700

    Update SubsetSum.java

--- src/SubsetSum.java
+++ src/SubsetSum.java
@@ -1,6 +1,24 @@
 package com.interview.dynamic;
 /*
  * http://www.geeksforgeeks.org/dynamic-programming-set-18-partition-problem/
+ 
+ Partition problem is to determine whether a given set can be partitioned into two subsets such that the sum of elements in both subsets is same.
+
+Examples
+
+arr[] = {1, 5, 11, 5}
+Output: true 
+The array can be partitioned as {1, 5, 5} and {11}
+
+arr[] = {1, 5, 3}
+Output: false 
+The array cannot be partitioned into equal sum sets.
+Following are the two main steps to solve this problem:
+1) Calculate sum of the array. If sum is odd, there can not be two subsets with equal sum, so return false.
+2) If sum of array elements is even, calculate sum/2 and find a subset of array with sum equal to sum/2.
+
+The first step is simple. The second step is crucial, it can be solved either using recursion or Dynamic Programming.
+
  */
 public class SubsetSum {
 
