d90588d5194f180809a5b22c8d709f2ea381c1f8
nunbitromance <sunghyolee@hotmail.com>
2015-07-05 14:40:25 -0700

    Create LongestCommonPrefix.java

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+Write a function to find the longest common prefix string amongst an array of strings.
+Solution
+
+There are two ways to get the longest common prefix:
+(DFS-like) Find a common prefix of the first two strings, compare that common prefix with the third one to find a common prefix of the first three, and so on, until find the common prefix of all strings.
+(BFS-like) Start from the first character. Compare whether the first character of all strings are the same, if so, move to next; otherwise, return the longest common prefix.
+In worst cases, both methods need to go through all strings. But on average, the second one would be slightly faster.
+
+// "flower", "floor", "flee", "fly" => "fl"
+
+ public String longestCommonPrefix(String[] strs) {  
+   if (strs.length == 0) return "";  
+   int index = 0;  
+   while (index < strs[0].length()) {  
+     char c = strs[0].charAt(index);  
+     for (int i=1; i<strs.length; ++i) {  
+       if (index >= strs[i].length() || strs[i].charAt(index) != c) {  
+         return strs[0].substring(0, index);  
+       }  
+     }  
+     index++;  
+   }  
+   return strs[0];  
+ }  
