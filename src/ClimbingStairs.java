14672717db1e544e0fffc454fb5a1daead3aea60
nunbitromance <sunghyolee@hotmail.com>
2015-07-21 23:37:32 -0700

    Create ClimbingStairs.java

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,29 @@
+/*
+Climbing Stairs
+
+You are climbing a stair case. It takes n steps to reach to the top. Each time you can either climb 1 or 2 steps. 
+In how many distinct ways can you climb to the top?
+*/
+
+ public int climbStairs(int n) {  
+   if (n <= 0) return 0;  
+   if (n == 1 || n == 2) return n;  
+   int[] ways = new int[n];  
+   ways[0] = 1; ways[1] = 2;  
+   for (int i=2; i<n; ++i) {  
+     ways[i] = ways[i-1] + ways[i-2];  
+   }  
+   return ways[n-1];  
+ }  
+
+// without extra array
+public int climbStairs(int n) {  
+   if (n <= 0) return 0;  
+   int p1 = 1, p2 = 1;  
+   for (int i=2; i<=n; ++i) {  
+     int temp = p2;  
+     p2 += p1;  
+     p1 = temp;  
+   }  
+   return p2;  
+ }  
