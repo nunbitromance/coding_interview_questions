700ddd8bc713babcccaf2aaf8f89da0e4c4306fc
nunbitromance <sunghyolee@hotmail.com>
2015-09-21 09:08:15 -0700

    Update IsBST.java

--- src/IsBST.java
+++ src/IsBST.java
@@ -1,66 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace CrackingTheCodingInterview
+public static bool isBST(Node root)
 {
-    public class Q_isBST
-    {
-        public class Node
-        {
-            public Node(int val)
-            {
-                Value = val;
-            }
+	return isBST(root, int.Max, 0);
+}
 
-            public Node Left { get; set; }
-            public Node Right { get; set; }
-            public int Value { get; set; }
-        }
+private static bool isBST(Node root, int max, int min)
+{
+	if (root == null)
+	{
+		return true;
+	}
+	
+	if (root.Value > max || root.Value < min)
+	{
+		return false;
+	}
+	
+	if (!isBST(root.getLeft(), root.getValue(), min))
+	{
+		return false;
+	}
 
-        public static bool isBST(Node root, ref Node prev)
-        {
-            if (root == null)
-            {
-                return true;
-            }
-
-            if (!isBST(root.Left, ref prev))
-            {
-                return false;
-            }
-
-            // Process information
-            if (prev != null && prev.Value > root.Value)
-            {
-                return false;
-            }
-            prev = root;
-
-            if (!isBST(root.Right, ref prev))
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        public static void Main(string[] args)
-        {
-            Node root = new Node(4);
-            root.Left = new Node(2);
-            root.Right = new Node(6);
-            root.Left.Left = new Node(1);
-            root.Left.Right = new Node(5);
-
-            Node prev = null;
-
-            bool isBst = Q_isBST.isBST(root, ref prev);
-
-            Console.ReadLine();
-        }
-
-
-    }
+	if (!isBST(root.getRight(), max, root.getValue())
+	{
+		return false;
+	}
+	
+	return true;
 }
