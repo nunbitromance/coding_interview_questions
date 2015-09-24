f236c598f62f5f8e7283df610c374af043353733
nunbitromance <sunghyolee@hotmail.com>
2015-09-23 22:28:46 -0700

    Update AirbnbInterviewQuestions.java

--- src/AirbnbInterviewQuestions.java
+++ src/AirbnbInterviewQuestions.java
@@ -10,3 +10,62 @@
 10. Store a set of sudden-death tournament results in a compact format (eg. a bit array) and a set of predicted match results (also in a bit array). Score the predictions, giving one point per correctly guessed match, without unpacking the bit array into a more convenient format (ie. you have to traverse the tree in-place). 
 11. Sort a list of numbers in which each number is at a distance k from its actual position  
 12. You have a plain with lots of rectangles on it, find out how many of them intersect  
+
+
+/**
+* returns a row of values as a list
+* returns null if you are past the end of the input stream
+*/
+public static List<String> parseLine(Reader r) throws Exception {
+    int ch = r.read();
+    while (ch == '\r') {
+        //ignore linefeed chars wherever, particularly just before end of file
+        ch = r.read();
+    }
+    if (ch<0) {
+        return null;
+    }
+    Vector<String> store = new Vector<String>();
+    StringBuffer curVal = new StringBuffer();
+    boolean inquotes = false;
+    boolean started = false;
+    while (ch>=0) {
+        if (inquotes) {
+            started=true;
+            if (ch == '\"') {
+                inquotes = false;
+            }
+            else {
+                curVal.append((char)ch);
+            }
+        }
+        else {
+            if (ch == '\"') {
+                inquotes = true;
+                if (started) {
+                    // if this is the second quote in a value, add a quote
+                    // this is for the double quote in the middle of a value
+                    curVal.append('\"');
+                }
+            }
+            else if (ch == ',') {
+                store.add(curVal.toString());
+                curVal = new StringBuffer();
+                started = false;
+            }
+            else if (ch == '\r') {
+                //ignore LF characters
+            }
+            else if (ch == '\n') {
+                //end of a line, break out
+                break;
+            }
+            else {
+                curVal.append((char)ch);
+            }
+        }
+        ch = r.read();
+    }
+    store.add(curVal.toString());
+    return store;
+}
