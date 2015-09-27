6739396db5e2247fa0b0c4b64213140d098927ce
nunbitromance <sunghyolee@hotmail.com>
2015-09-27 12:16:47 -0700

    Update JustifyText.java

--- src/JustifyText.java
+++ src/JustifyText.java
@@ -25,24 +25,47 @@ Corner Cases:
 A line other than the last line might contain only one word. What should you do in this case?
 In this case, that line should be left-justified.
 */
-public static void JustifyText(string[] words, int n)
-{
-	StringBuilder sb = new StringBuilder();
-	
-	int length = 0;
-	for (int i = 0; i < words.Length; i++)
-	{
-		for (int j = i; j < words.Length; j++)
-		{
-			length = length + 1 + words[j].Length;
-			
-			if (length > n)
-			{
-				break;
-			}
-		}
-		
-		length = length - words[j].Length;
-		n - 
-	}
+public class Solution {
+    public List<String> fullJustify(String[] words, int L) {
+        List<String> lines = new ArrayList<String>();
+
+        int index = 0;
+        while (index < words.length) {
+            int count = words[index].length();
+            int last = index + 1;
+            while (last < words.length) {
+                if (words[last].length() + count + 1 > L) break;
+                count += words[last].length() + 1;
+                last++;
+            }
+
+            StringBuilder builder = new StringBuilder();
+            int diff = last - index - 1;
+            // if last line or number of words in the line is 1, left-justified
+            if (last == words.length || diff == 0) {
+                for (int i = index; i < last; i++) {
+                    builder.append(words[i] + " ");
+                }
+                builder.deleteCharAt(builder.length() - 1);
+                for (int i = builder.length(); i < L; i++) {
+                    builder.append(" ");
+                }
+            } else {
+                // middle justified
+                int spaces = (L - count) / diff;
+                int r = (L - count) % diff;
+                for (int i = index; i < last; i++) {
+                    builder.append(words[i]);
+                    if (i < last - 1) {
+                        for (int j = 0; j <= (spaces + ((i - index) < r ? 1 : 0)); j++) {
+                            builder.append(" ");
+                        }
+                    }
+                }
+            }
+            lines.add(builder.toString());
+            index = last;
+    }
+    return lines;
+    }
 }
