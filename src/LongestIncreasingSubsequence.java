c87516705d9f5b4eb110075b3989d928a78775ef
nunbitromance <sunghyolee@hotmail.com>
2015-09-28 22:14:17 -0700

    Update LongestIncreasingSubsequence.java

--- src/LongestIncreasingSubsequence.java
+++ src/LongestIncreasingSubsequence.java
@@ -3,30 +3,46 @@
 http://www.geeksforgeeks.org/dynamic-programming-set-3-longest-increasing-subsequence/
 // OPT(i) = 1 + max{OPT(j)} for j < i and A[i] >= A[j]
 // For example, length of LIS for { 10, 22, 9, 33, 21, 50, 41, 60, 80 } is 6 and LIS is {10, 22, 33, 50, 60, 80}.
-public static int longestIncreasingSubsequence(int[] array)
-{
-	if (array == null || array.length == 0)
-	{
-		return 0;
-	}
-
-	int[] maxLenghth = new int[array.length];
-	maxLength[0] = 0;
-	
-	for (int i = 1; i < maxLength.length; i++)
-	{
-		int maxLength = 0;
-		
-		for (int j = 0; j < i; j++)
-		{
-			if (maxLength[j] > maxLength && array[j] < array[i])
-			{
-				maxLength = maxLength[j];
-			}
-		}
-		
-		maxLength[i] = maxLength + 1;
-	}
-    	
-	return maxLength[array.length - 1];
-}
+    /**
+     * DP way of solving LIS
+     */
+    public int longestSubsequenceWithActualSolution(int arr[]){
+        int T[] = new int[arr.length];
+        int actualSolution[] = new int[arr.length];
+        for(int i=0; i < arr.length; i++){
+            T[i] = 1;
+            actualSolution[i] = i;
+        }
+        
+        for(int i=1; i < arr.length; i++){
+            for(int j=0; j < i; j++){
+                if(arr[i] > arr[j]){
+                    if(T[j] + 1 > T[i]){
+                        T[i] = T[j] + 1;
+                        //set the actualSolution to point to guy before me
+                        actualSolution[i] = j;
+                    }
+                }
+            }
+        }
+        
+        //find the index of max number in T 
+        int maxIndex = 0;
+        for(int i=0; i < T.length; i++){
+            if(T[i] > T[maxIndex]){
+                maxIndex = i;
+            }
+        }
+        
+        //lets print the actual solution
+        int t = maxIndex;
+        int newT = maxIndex;
+        do{
+            t = newT;
+            System.out.print(arr[t] + " ");
+            newT = actualSolution[t];
+        }while(t != newT);
+        System.out.println();
+ 
+        return T[maxIndex];
+    }
