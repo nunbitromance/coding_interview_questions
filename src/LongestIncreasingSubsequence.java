d6bbc38fd447d3a49509938f157f19650c5f3c23
nunbitromance <sunghyolee@hotmail.com>
2015-10-06 21:12:38 -0700

    Update LongestIncreasingSubsequence.java

--- src/LongestIncreasingSubsequence.java
+++ src/LongestIncreasingSubsequence.java
@@ -6,43 +6,45 @@ http://www.geeksforgeeks.org/dynamic-programming-set-3-longest-increasing-subseq
     /**
      * DP way of solving LIS
      */
-    public int longestSubsequenceWithActualSolution(int arr[]){
-        int T[] = new int[arr.length];
-        int actualSolution[] = new int[arr.length];
-        for(int i=0; i < arr.length; i++){
-            T[i] = 1;
-            actualSolution[i] = i;
-        }
+
+public class LongestIncreasingSubsequence {
+
+    public void longestIncreaseingSubsequence(int[] arr) {
+        int[] opt = new int[arr.length];
+        opt[0] = 1;
+        int[] prev = new int[arr.length];
+        prev[0] = -1;
         
-        for(int i=1; i < arr.length; i++){
-            for(int j=0; j < i; j++){
-                if(arr[i] > arr[j]){
-                    if(T[j] + 1 > T[i]){
-                        T[i] = T[j] + 1;
-                        //set the actualSolution to point to guy before me
-                        actualSolution[i] = j;
-                    }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && opt[j] + 1 > opt[i]) {
+                    opt[i] = opt[j] + 1;
+                    prev[i] = j;
                 }
             }
         }
         
-        //find the index of max number in T 
         int maxIndex = 0;
-        for(int i=0; i < T.length; i++){
-            if(T[i] > T[maxIndex]){
+        for (int i = 0; i < arr.length; i++) {
+            if (opt[i] > opt[maxIndex]) {
                 maxIndex = i;
             }
         }
         
-        //lets print the actual solution
         int t = maxIndex;
-        int newT = maxIndex;
-        do{
-            t = newT;
-            System.out.print(arr[t] + " ");
-            newT = actualSolution[t];
-        }while(t != newT);
-        System.out.println();
- 
-        return T[maxIndex];
+        while (t >= 0) {
+            System.out.println(arr[t]);
+            t = prev[t];
+        }
     }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int[] arr = new int[]{ 10, 22, 9, 33, 21, 50, 41, 60, 80 };
+        new LongestIncreasingSubsequence().longestIncreaseingSubsequence(arr);
+    }
+
+}
