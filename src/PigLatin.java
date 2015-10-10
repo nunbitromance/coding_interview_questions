a729162376b2c1aee84ba6dbe6367f7025b6b8f6
nunbitromance <sunghyolee@hotmail.com>
2015-10-09 17:47:29 -0700

    Update PigLatin.java

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -3,28 +3,48 @@
     2) if starts with vowel, add "hay" to end
 */
 
-import java.util.Scanner;
 
-public class pigLatin {
-   public static void main(String[] args) {
-   
-      Scanner kb = new Scanner(System.in);
-      String word, pig;
-      char first;
-      
-      System.out.print("enter word: ");
-      word = kb.next();
-      //NOT: word.toLowerCase();
-      word = word.toLowerCase();
-      System.out.println(word);
-      
-      first = word.charAt(0);
-      if (first == 'a' ||  first == 'e' || first == 'i' || 
-         first == 'o' || first == 'u')  // vowel
-         pig = word + "hay";      
-      else
-         pig = word.substring(1) + word.charAt(0) + "ay";
-      
-      System.out.println("pig-latin version: " + pig);
-   }
+public class PigLatin {
+
+    public String encode(String kb) {
+        String word, pig;
+        char first;
+
+        word = kb;
+        //NOT: word.toLowerCase();
+        word = word.toLowerCase();    
+        first = word.charAt(0);
+        if (first == 'a' ||  first == 'e' || first == 'i' || 
+           first == 'o' || first == 'u')  // vowel
+           pig = word + "hay";      
+        else
+           pig = word.substring(1) + word.charAt(0) + "ay";
+        return pig;
+        
+    }
+    
+    public String decode(String pig) {
+        if (pig.endsWith("hay")) {
+            return pig.substring(0, pig.length() - 3);
+        } else if (pig.endsWith("ay")) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(pig.charAt(pig.length()-3));
+            sb.append(pig.substring(0, pig.length() - 3));
+            return sb.toString();
+        }
+        return null;
+    }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String word = "cabe";
+        System.out.println("normal version: " + word);
+        String pig = new PigLatin().encode(word);
+        System.out.println("pig-latin version: " + pig);
+        String word2 = new PigLatin().decode(pig);
+        System.out.println("decoded version: " + word2);
+    }
+
 }
