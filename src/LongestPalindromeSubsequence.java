104ffd2760dec18e44b556386ec3febd837d2045
nunbitromance <sunghyolee@hotmail.com>
2015-10-15 22:57:42 -0700

    Update LongestPalindromeSubsequence.java

--- src/LongestPalindromeSubsequence.java
+++ src/LongestPalindromeSubsequence.java
@@ -1,49 +1,44 @@
-package com.interview.dynamic;
-
 /**
  * http://www.geeksforgeeks.org/dynamic-programming-set-12-longest-palindromic-subsequence/
  */
+package practice;
+
 public class LongestPalindromicSubsequence {
 
-    public int calculate(char []str){
-        int T[][] = new int[str.length+1][str.length+1];
-        for(int i=0; i < str.length; i++){
-            T[1][i] = 1;
+    public int lps(String s) {
+        // validation
+        int[][] opt = new int[s.length()][s.length()];
+        
+        for (int i = 0; i < s.length(); i++) {
+            opt[i][0] = 1;
+            opt[0][i] = 1;
         }
-        for(int c = 2; c <= str.length; c++){
-            for(int i = 1; i <= str.length-c+1; i++){
-                if(str[i-1] == str[i-1 + c-1] && c == 2){
-                    T[c][i] = 2;
-                }else if(str[i-1] == str[i+c-2]){
-                    T[c][i] = T[c-2][i+1] + 2;
-                }else{
-                    T[c][i] = Math.max(T[c-1][i+1], T[c-1][i]);
+        
+        for (int i = 0; i < s.length() - 1; i++) {
+            opt[i][i+1] = s.charAt(i) == s.charAt(i+1) ? 2 : 1;
+        }
+        
+        for (int l = 3; l <= s.length(); l++) {
+            for (int i = 0; i < s.length() - l + 1; i++) {
+                int j = i + l - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    opt[i][j] = opt[i+1][j-1] + 2;
+                } else {
+                    opt[i][j] = Math.max(opt[i+1][j], opt[i][j-1]);
                 }
             }
         }
-        return T[str.length][1];
+        
+        return opt[0][s.length() - 1];
     }
     
-    public int calculateRecursive(char str[],int start,int len){
-        if(len == 1){
-            return 1;
-        }
-        if(len ==0){
-            return 0;
-        }
-        if(str[start] == str[start+len-1]){
-            return 2 + calculateRecursive(str,start+1,len-2);
-        }else{
-            return Math.max(calculateRecursive(str, start+1, len-1), calculateRecursive(str, start, len-1));
-        }
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String s = "GEEKSFORGEEKS";
+        System.out.println(new LongestPalindromicSubsequence().lps(s));
     }
-    
-    public static void main(String args[]){
-        LongestPalindromicSubsequence lps = new LongestPalindromicSubsequence();
-        String str = "GEEKSFORGEEKS";
-        int  r = lps.calculate(str.toCharArray());
-        int r1 = lps.calculateRecursive(str.toCharArray(), 0, str.length());
-        System.out.print(r + " " + r1);
-    }
-    
+
 }
