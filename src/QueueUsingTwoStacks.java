839d2be42f02f1de32a310564233333ab16da8ea
nunbitromance <sunghyolee@hotmail.com>
2017-06-06 00:01:36 -0700

    Update QueueUsingTwoStacks.java

--- src/QueueUsingTwoStacks.java
+++ src/QueueUsingTwoStacks.java
@@ -16,10 +16,11 @@ public class MyQueue<T>
 	
 	public T dequeue()
 	{
-		while (s1.isEmpty() == false)
-		{
-			s2.push(s1.pop());
-		}
+		if (s2.isEmpty()) {
+			while (!s1.isEmpty()) {
+			      s2.push(s1.pop());
+			}
+    		}  
 		return s2.pop();
 	}
 	
