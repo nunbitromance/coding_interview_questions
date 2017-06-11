aeb493ac6ae7dbdf467275e6f5733ec4c4f4991d
nunbitromance <sunghyolee@hotmail.com>
2017-06-10 23:07:25 -0700

    Create LevelOrderTraversal.java

--- src/LevelOrderTraversal.java
+++ src/LevelOrderTraversal.java
@@ -25,6 +25,35 @@
         return result;
     }
 
+    /**
+     * Use one queue and delimiter to print level by level
+     */
+    public void levelByLevelOneQueueUsingDelimiter(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<Node>();
+        q.offer(root);
+        q.offer(null);
+        while (!q.isEmpty()) {
+            root = q.poll();
+            if (root != null) {
+                System.out.print(root.data + " ");
+                if (root.left != null) {
+                    q.offer(root.left);
+                }
+                if (root.right != null) {
+                    q.offer(root.right);
+                }
+            } else {
+                if (!q.isEmpty()) {
+                    System.out.println();
+                    q.offer(null);
+                }
+            }
+        }
+    }
+
 
     /**
      * Use one queue and count to print level by level
