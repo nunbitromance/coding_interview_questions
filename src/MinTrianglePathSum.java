20abedf0beffc33a6fa9e252070e40abd4d6b91d
nunbitromance <sunghyolee@hotmail.com>
2017-06-27 00:00:08 -0700

    Update MinTrianglePathSum.java

--- src/MinTrianglePathSum.java
+++ src/MinTrianglePathSum.java
@@ -22,3 +22,30 @@ public int minPath(List<List<Integer>> triangle)
  
     return total[0];
 }
+
+
+// max sum
+
+//  Function for finding maximum sum
+int maxPathSum(int tri[][N], int m, int n)
+{
+     // loop for bottom-up calculation
+     for (int i=m-1; i>=0; i--)
+     {
+        for (int j=0; j<=i; j++)
+        {
+            // for each element, check both
+            // elements just below the number
+            // and below right to the number
+            // add the maximum of them to it
+            if (tri[i+1][j] > tri[i+1][j+1])
+                tri[i][j] += tri[i+1][j];
+            else
+                tri[i][j] += tri[i+1][j+1];
+        }
+     }
+ 
+     // return the top element
+     // which stores the maximum sum
+     return tri[0][0];
+}
