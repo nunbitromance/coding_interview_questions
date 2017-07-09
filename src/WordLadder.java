ab7095b5566b00686f2bade90f08550b9c389f17
nunbitromance <sunghyolee@hotmail.com>
2017-07-09 13:53:30 -0700

    Update WordLadder.java

--- src/WordLadder.java
+++ src/WordLadder.java
@@ -1,3 +1,16 @@
+/*Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+
+Only one letter can be changed at a time.
+Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+For example,
+
+Given:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log","cog"]
+As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5.*/
+
 public class Solution {
     public int ladderLength(String start, String end, Set<String> dict) {
         Queue<String> queue = new LinkedList<String>();
