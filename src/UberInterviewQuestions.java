a9fd628d0f7e67189f9f2c1f3f8cbe3798db5621
nunbitromance <sunghyolee@hotmail.com>
2017-07-15 00:08:23 -0700

    Update UberInterviewQuestions.java

--- src/UberInterviewQuestions.java
+++ src/UberInterviewQuestions.java
@@ -34,3 +34,126 @@ Serializing and deserializing binary trees
 Implement a ring buffer.
 Insert a value into a sorted circular list given one node(any) in the circular list.
 Implementing getNBytes(int N, char* buffer) from get4Bytes(char* buffer).
+
+import java.io.*;
+import java.util.*;
+import java.util.Date;
+
+/*
+ * To execute Java, please define "static void main" on a class
+ * named Solution.
+ *
+ * If you need more classes, simply define them inline.
+
+
+We own a chain of pizza restaurants. 
+
+We need to build:
+
+/timeToNextPizza/<pizza type> : returns the earliest  time that ANY location will produce that kind of pizza 
+
+
+
+We have access to 2 endpoints (external APIs):
+
+
+1.  /locations/<pizza type> : returns all locations that produce that kind of pizza
+
+
+
+e.g.: /locations/peperroni: [TrainStation, CivicCenter]
+
+
+
+
+2. /timeToNextPizza/<location>/<pizza type> : returns the next time that location will produce that kind of pizza in minutes
+10:00am TrainStation peperroni: 7
+10:01am                       : 6
+10:07                           0
+10:08                           19
+
+
+
+e.g.:
+
+/timeToNextPizza/TrainStation/peperroni : 7
+
+/timeToNextPizza/CivicCenter/peperroni : 13
+
+Coming back to the endpoint we need to expose, 
+for above values:
+
+/timeToNextPizza/peperroni : 7
+
+
+ */
+
+class CacheKey {
+  String location;
+  String pizzaType;
+}
+
+class CacheValue {
+  public Date originalTime;
+  public int min;
+}
+
+class Solution {
+  
+  private Map<CacheKey, CacheValue> cache = new HashMap<CacheKey, CacheValue>(); 
+  
+  public List<String> getLocations(String pizzaType) {
+    List<String> result = new ArrayList<String>();
+    result.add("TrainStation");
+    result.add("CivicCenter");
+    return result;
+  }
+  
+  public Integer getNextTime(String location, String pizzaType) {
+    
+    if (location.equals("TrainStation")) {
+      return 7;
+    } else if (location.equals("CivicCenter")) {
+      return 6;
+    }
+    return -1;
+  }
+  
+  public Integer getNextEarliestTime(String pizzaType) {
+    
+    List<String> locations = getLocations(pizzaType);
+    
+    int minNextTime = Integer.MAX_VALUE;
+    for (String location: locations) {
+      
+      CacheValue val = null;
+      CacheKey key = new CacheKey(location, pizzaType);
+      int nextTime = -1;
+      if (cache.containsKey(key)) {
+        val = cache.get(key);
+        if ((val.min -  (new Date() - val.originalTime).minuteOfDay()) > 0) {
+          nextTime = val.min -  (Date() - val.originalTime).minuteOfDay();
+        }
+      }
+      
+      if (nextTime == -1) {
+        nextTime = getNextTime(location, pizzaType);
+        val = new CacheValue(DateTime.now(), nextTime);
+        cache.put(key, val);
+      }
+
+      minNextTime = Math.min(nextTime, minNextTime);
+    }
+    
+    return minNextTime;
+  }
+  
+  
+  public static void main(String[] args) {
+    
+    Solution s = new Solution();
+    int val = s.getNextEarliestTime("peperoni");
+    System.out.println("earliest time" + val);
+  }
+}
+
