3c1f6dd1e21a2306c1a4a2f7e07cccd0b411ef0a
nunbitromance <sunghyolee@hotmail.com>
2017-07-15 19:16:02 -0700

    Update MaxSubarrayInCircularArray.java

--- src/MaxSubarrayInCircularArray.java
+++ src/MaxSubarrayInCircularArray.java
@@ -6,7 +6,9 @@ now in first run of kadane algorithm we look for subarray with max sum in first 
 
 now in for loop we add all the elements and change sign so after for loop our array is -12 5 -4 8 -11 and sum is 14
 
-when we run again kadane algorithm it will give that part of array which has actually min sum in this case -9 .so kadane wil return 9 as we have changed sign of all the array and then we remove that part from sum which has minimum sum max_wrap + kadane(a, n);
+when we run again kadane algorithm it will give that part of array which has actually min sum in this case -9 .
+
+so kadane wil return 9 as we have changed sign of all the array and then we remove that part from sum which has minimum sum max_wrap + kadane(a, n);
 
 then we return max of first run of kadane and ans we got after removing that part which has minimum sum.
 */
