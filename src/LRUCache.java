26eb4d3447df7ee0f088c971b4a25eb844658fb5
nunbitromance <sunghyolee@hotmail.com>
2017-07-16 13:43:58 -0700

    Update LRUCache.java

--- src/LRUCache.java
+++ src/LRUCache.java
@@ -147,3 +147,20 @@ class CacheNode<K,V> {
     }
     
 }
+
+public class Cache extends
+    LinkedHashMap
+{
+  private final int capacity;
+
+  public Cache(int capacity)
+  {
+    super(capacity + 1, 1.1f, true);
+    this.capacity = capacity;
+  }
+
+  protected boolean removeEldestEntry(Entry eldest)
+  {
+    return size() > capacity;
+  }
+}
