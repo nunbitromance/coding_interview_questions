335632c29747b4ad44d98813a24a9a0ea6c036c0
nunbitromance <sunghyolee@hotmail.com>
2017-07-16 21:02:36 -0700

    Update GetPrimeNumber.java

--- src/GetPrimeNumber.java
+++ src/GetPrimeNumber.java
@@ -28,3 +28,28 @@ boolean isPrime(int n) {
     }
     return true;
 }
+
+import java.util.Arrays;
+//global array just to keep track of it in this example,
+//but you can easily do this within another function.
+
+// will contain true or false values for the first 10,000 integers
+boolean[] primes=new boolean[10000];
+//set up the primesieve
+public void fillSieve() {
+    Arrays.fill(primes,true);        // assume all integers are prime.
+    primes[0]=primes[1]=false;       // we know 0 and 1 are not prime.
+    for (int i=2;i<primes.length;i++) {
+        //if the number is prime,
+        //then go through all its multiples and make their values false.
+        if(primes[i]) {
+            for (int j=2;i*j<primes.length;j++) {
+                primes[i*j]=false;
+            }
+        }
+    }
+}
+
+public boolean isPrime(int n) {
+    return primes[n]; //simple, huh?
+}
