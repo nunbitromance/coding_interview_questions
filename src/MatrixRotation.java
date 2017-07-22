07fa25bbcb626ace6cb729d5c434974da6e84448
nunbitromance <sunghyolee@hotmail.com>
2017-07-22 16:25:27 -0700

    Update MatrixRotation.java

--- src/MatrixRotation.java
+++ src/MatrixRotation.java
@@ -32,23 +32,20 @@ public void rotate(int[][] matrix) {
 }
 
 public static void rotate(int[][] matrix, int n) {
-2 for (int layer = 0; layer < n / 2; ++layer) {
-3 int first = layer;
-4 int last = n - 1 - layer;
-5 for(int i = first; i < last; ++i) {
-6 int offset = i - first;
-7 int top = matrix[first][i]; // save top
-8 // left -> top
-9 matrix[first][i] = matrix[last-offset][first];
-10
-11 // bottom -> left
-12 matrix[last-offset][first] = matrix[last][last - offset];
-13
-14 // right -> bottom
-15 matrix[last][last - offset] = matrix[i][last];
-16
-17 // top -> right
-18 matrix[i][last] = top; // right <- saved top
-19 }
-20 }
-21 }
+	for (int layer = 0; layer < n / 2; ++layer) {
+		int first = layer;
+		int last = n - 1 - layer;
+		for (int i = first; i < last; ++i) {
+			int offset = i - first;
+			int top = matrix[first][i]; // save top
+			// left -> top
+			matrix[first][i] = matrix[last-offset][first];
+			// bottom -> left
+			matrix[last-offset][first] = matrix[last][last - offset];
+			// right -> bottom
+			matrix[last][last - offset] = matrix[i][last];
+			// top -> right
+			matrix[i][last] = top; // right <- saved top
+		}
+	}
+}
