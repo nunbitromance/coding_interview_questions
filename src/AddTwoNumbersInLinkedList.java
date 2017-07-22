ce9af4db46e2e25c7bbebc3d9fcfd71d362dfd55
nunbitromance <sunghyolee@hotmail.com>
2017-07-22 15:19:19 -0700

    Create AddTwoNumbersInLinkedList.java

---
+++ src/AddTwoNumbersInLinkedList.java
@@ -0,0 +1,29 @@
+/*
+
+Complexity Analysis
+
+What if the the digits in the linked list are stored in non-reversed order? For example:
+
+(3→4→2)+(4→6→5)=8→0→7
+
+*/
+
+public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode dummyHead = new ListNode(0);
+    ListNode p = l1, q = l2, curr = dummyHead;
+    int carry = 0;
+    while (p != null || q != null) {
+        int x = (p != null) ? p.val : 0;
+        int y = (q != null) ? q.val : 0;
+        int sum = carry + x + y;
+        carry = sum / 10;
+        curr.next = new ListNode(sum % 10);
+        curr = curr.next;
+        if (p != null) p = p.next;
+        if (q != null) q = q.next;
+    }
+    if (carry > 0) {
+        curr.next = new ListNode(carry);
+    }
+    return dummyHead.next;
+}
