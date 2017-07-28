f99e88624d641f4a87ca03b09d9f67219500fca5
nunbitromance <sunghyolee@hotmail.com>
2017-07-28 09:20:41 -0700

    Update NextGreater.java

--- src/NextGreater.java
+++ src/NextGreater.java
@@ -21,23 +21,68 @@ d) For the input array [13, 7, 6, 12}, the next greater elements for each elemen
    6       -->     12
    12     -->     -1
 */
-public static void NextGreaterElement(int[] a)
-{
-	Stack<int> s = new Stack();
-	s.Push(a[0]);
-	int index = 0;
-	int[] output = new int[a.Length];
-	for (int i = 1; i < a.Length; i++)
-	{
-		if (a[i] < s.Peek())
-		{
-			while (s.Peek() < a[i])
-			{
-				Print(s.Pop(), a[i]);
-			}
-		}
-		s.Push(a[i]);
-	}
-	
-	Print(s.Pop(), -1);
-} 
+/* prints element and NGE pair for 
+       all elements of arr[] of size n */
+    public void printNGE(int arr[], int n) 
+    {
+        int i = 0;
+        stack s = new stack();
+        s.top = -1;
+        int element, next;
+ 
+        /* push the first element to stack */
+        s.push(arr[0]);
+ 
+        // iterate for rest of the elements
+        for (i = 1; i < n; i++) 
+        {
+            next = arr[i];
+ 
+            if (s.isEmpty() == false) 
+            {
+                 
+                // if stack is not empty, then 
+                // pop an element from stack
+                element = s.pop();
+ 
+                /* If the popped element is smaller than 
+                   next, then a) print the pair b) keep 
+                   popping while elements are smaller and 
+                   stack is not empty */
+                while (element < next) 
+                {
+                    System.out.println(element + " -- " + next);
+                    if (s.isEmpty() == true)
+                        break;
+                    element = s.pop();
+                }
+ 
+                /* If element is greater than next, then 
+                   push the element back */
+                if (element > next)
+                    s.push(element);
+            }
+ 
+            /* push next to stack so that we can find next
+               greater for it */
+            s.push(next);
+        }
+ 
+        /* After iterating over the loop, the remaining 
+           elements in stack do not have the next greater 
+           element, so print -1 for them */
+        while (s.isEmpty() == false) 
+        {
+            element = s.pop();
+            next = -1;
+            System.out.println(element + " -- " + next);
+        }
+    }
+ 
+    public static void main(String[] args) 
+    {
+        int arr[] = { 11, 13, 21, 3 };
+        int n = arr.length;
+        printNGE(arr, n);
+    }
+}
