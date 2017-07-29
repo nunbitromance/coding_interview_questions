d67daf9fb2b89da9d41812c3d574ee0a792214d3
nunbitromance <sunghyolee@hotmail.com>
2017-07-29 16:38:06 -0700

    Update MinPalindromePartition.java

--- src/MinPalindromePartition.java
+++ src/MinPalindromePartition.java
@@ -50,3 +50,41 @@ minPalPartion(str, i, j) = Min { minPalPartion(str, i, k) + 1 +
         }
         return cut[str.length() - 1];
     }
+
+public static List<String> palindromePartitioning(String s) {
+ 
+	List<String> result = new ArrayList<String>();
+ 
+	if (s == null)
+		return result;
+ 
+	if (s.length() <= 1) {
+		result.add(s);
+		return result;
+	}
+ 
+	int length = s.length();
+ 
+	int[][] table = new int[length][length];
+ 
+	// l is length, i is index of left boundary, j is index of right boundary
+	for (int l = 1; l <= length; l++) {
+		for (int i = 0; i <= length - l; i++) {
+			int j = i + l - 1;
+			if (s.charAt(i) == s.charAt(j)) {
+				if (l == 1 || l == 2) {
+					table[i][j] = 1;
+				} else {
+					table[i][j] = table[i + 1][j - 1];
+				}
+				if (table[i][j] == 1) {
+					result.add(s.substring(i, j + 1));
+				}
+			} else {
+				table[i][j] = 0;
+			}
+		}
+	}
+ 
+	return result;
+}
