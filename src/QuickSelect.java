e672e22bf987f619180e0ac0b90486ae3d141d1f
nunbitromance <sunghyolee@hotmail.com>
2017-07-30 14:34:46 -0700

    Update QuickSelect.java

--- src/QuickSelect.java
+++ src/QuickSelect.java
@@ -108,3 +108,71 @@ public static int selectKth(int[] arr, int k) {
  
  return arr[k];
 }
+
+/*Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+For example, given [3,2,1,5,6,4] and k = 2, return 5.
+
+Note: You may assume k is always valid, 1 ≤ k ≤ array's length.
+
+Java Solution 1 - Sorting
+
+public int findKthLargest(int[] nums, int k) {
+    Arrays.sort(nums);
+    return nums[nums.length-k];
+}
+Time is O(nlog(n)).
+
+Java Solution 2 - Quick Sort
+
+This problem can also be solved by using quickselect, which is similar to quicksort.
+*/
+public int findKthLargest(int[] nums, int k) {
+	if (k < 1 || nums == null) {
+		return 0;
+	}
+ 
+	return getKth(nums.length - k +1, nums, 0, nums.length - 1);
+}
+ 
+public int getKth(int k, int[] nums, int start, int end) {
+ 
+	int pivot = nums[end];
+ 
+	int left = start;
+	int right = end;
+ 
+	while (true) {
+ 
+		while (nums[left] < pivot && left < right) {
+			left++;
+		}
+ 
+		while (nums[right] >= pivot && right > left) {
+			right--;
+		}
+ 
+		if (left == right) {
+			break;
+		}
+ 
+		swap(nums, left, right);
+	}
+ 
+	swap(nums, left, end);
+ 
+	if (k == left + 1) {
+		return pivot;
+	} else if (k < left + 1) {
+		return getKth(k, nums, start, left - 1);
+	} else {
+		return getKth(k, nums, left + 1, end);
+	}
+}
+ 
+public void swap(int[] nums, int n1, int n2) {
+	int tmp = nums[n1];
+	nums[n1] = nums[n2];
+	nums[n2] = tmp;
+}
+Average case time is O(n), worst case time is O(n^2).
