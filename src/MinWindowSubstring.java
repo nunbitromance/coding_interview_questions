682960545b801ed4ae3c2586639e30b210628657
nunbitromance <sunghyolee@hotmail.com>
2017-08-05 12:24:13 -0700

    Update MinWindowSubstring.java

--- src/MinWindowSubstring.java
+++ src/MinWindowSubstring.java
@@ -10,113 +10,79 @@ Minimum window is "BANC".
 Note:
 If there is no such window in S that covers all characters in T, return the emtpy string "".
 
-If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in S.
-1- First check if length of string is less than
-   the length of given pattern, if yes
-       then "no such window can exist ".
-2- Store the occurrence of characters of given 
-   pattern in a hash_pat[].
-3- Start matching the characters of pattern with 
-   the characters of string i.e. increment count 
-   if a character matches
-4- Check if (count == length of pattern ) this 
-   means a window is found
-5- If such window found, try to minimize it by 
-   removing extra characters from beginning of
-   current window.
-6- Update min_length.
-7- Print the minimum length window.pot.  
-  */  
-// Java program to find smallest window containing
-// all characters of a pattern.
+Analysis
+
+We are required to implement a O(n)
+O
+(
+n
+)
+ algorithm. We can achieve it by using two pointers. We will use pointer “start” and pointer “end”, which indicates the substring we are processing.
+
+Firstly, we will move the “end” pointer right, until we find all characters. But how can we know that we have found all the characters? There may be duplicate characters in string T. So I use a HashMap to save the number of each characters in string T. Another HashMap is used to save the number of characters we found. When we move pointer “end”, we will check if the number of this character is smaller than we need to found. If it is, we will increase one to a total counter and increase one to the number of this character. Otherwise only the number of this character is increased.
+
+When the total counter equals to length of T, we know that we have found all characters. Now we can move the “start” pointer as right as possible. We can move it when it pointing to a character that is not in T, or even it is in T, but the number of this character is larger than the number we need to find. In other words, we move “start” pointer when the substring from “start” to “end” does contains all characters in T.
+
+We can compare the value end–start+1
+e
+n
+d
+–
+s
+t
+a
+r
+t
++
+1
+, which is the length of a substring that contains all characters in T, to the minimum length. If it’s shorter, we can update the minimum length.
+
+In fact, we can also use an array to save the number of each characters. It could be faster than using HashMap. You can check the code here: http://leetcode.com/2010/11/finding-minimum-window-in-s-which.html.*/
  
-public class GFG 
-{
-    static final int no_of_chars = 256;
-     
-    // Function to find smallest window containing
-    // all characters of 'pat'
-    static String findSubString(String str, String pat)
-    {
-        int len1 = str.length();
-        int len2 = pat.length();
-      
-        // check if string's length is less than pattern's
-        // length. If yes then no such window can exist
-        if (len1 < len2)
-        {
-            System.out.println("No such window exists");
-            return "";
+public class Solution {
+    public String minWindow(String S, String T) {
+        HashMap<Character, Integer> dict = new HashMap<>();
+        for (int i = 0; i < T.length(); i++) {
+            char c = T.charAt(i);
+            if (!dict.containsKey(c))
+                dict.put(c, 1);
+            else
+                dict.put(c, dict.get(c) + 1);
         }
-      
-        int hash_pat[] = new int[no_of_chars];
-        int hash_str[] = new int[no_of_chars];
-      
-        // store occurrence ofs characters of pattern
-        for (int i = 0; i < len2; i++)
-            hash_pat[pat.charAt(i)]++;
-      
-        int start = 0, start_index = -1, min_len = Integer.MAX_VALUE;
-      
-        // start traversing the string
-        int count = 0;  // count of characters
-        for (int j = 0; j < len1 ; j++)
-        {
-            // count occurrence of characters of string
-            hash_str[str.charAt(j)]++;
-      
-            // If string's char matches with pattern's char
-            // then increment count
-            if (hash_pat[str.charAt(j)] != 0 &&
-                hash_str[str.charAt(j)] <= hash_pat[str.charAt(j)] )
-                count++;
-      
-            // if all the characters are matched
-            if (count == len2)
-            {
-                // Try to minimize the window i.e., check if
-                // any character is occurring more no. of times
-                // than its occurrence  in pattern, if yes
-                // then remove it from starting and also remove
-                // the useless characters.
-                while ( hash_str[str.charAt(start)] > hash_pat[str.charAt(start)]
-                       || hash_pat[str.charAt(start)] == 0)
-                {
-      
-                    if (hash_str[str.charAt(start)] > hash_pat[str.charAt(start)])
-                        hash_str[str.charAt(start)]--;
-                    start++;
-                }
-      
-                // update window size
-                int len_window = j - start + 1;
-                if (min_len > len_window)
-                {
-                    min_len = len_window;
-                    start_index = start;
+        HashMap<Character, Integer> found = new HashMap<>();
+        int foundCounter = 0;
+        int start = 0;
+        int end = 0;
+        int min = Integer.MAX_VALUE;
+        String minWindow = "";
+        while (end < S.length()) {
+            char c = S.charAt(end);
+            if (dict.containsKey(c)) {
+                if (found.containsKey(c)) {
+                    if (found.get(c) < dict.get(c))
+                        foundCounter++;
+                    found.put(c, found.get(c) + 1);
+                } else {
+                    found.put(c, 1);
+                    foundCounter++;
                 }
             }
+            if (foundCounter == T.length()) {
+                //When foundCounter equals to T.length(), in other words, found all characters.
+                char sc = S.charAt(start);
+                while (!found.containsKey(sc) || found.get(sc) > dict.get(sc)) {
+                    if (found.containsKey(sc) && found.get(sc) > dict.get(sc))
+                        found.put(sc, found.get(sc) - 1);
+                    start++;
+                    sc = S.charAt(start);
+                }
+                if (end - start + 1 < min) {
+                    minWindow = S.substring(start, end + 1);
+                    min = end - start + 1;
+                }
+            }
+            end++;
         }
-      
-        // If no window found
-        if (start_index == -1)
-        {
-           System.out.println("No such window exists");
-           return "";
-        }
-      
-        // Return substring starting from start_index
-        // and length min_len
-        return str.substring(start_index, start_index + min_len);
-    }
-     
-    // Driver Method
-    public static void main(String[] args)
-    {
-        String str = "this is a test string";
-        String pat = "tist";
-      
-       System.out.print("Smallest window is :  \n" +
-                        findSubString(str, pat));
+        return minWindow;
     }
 }
