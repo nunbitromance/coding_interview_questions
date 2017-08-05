775c3cdfb3fa2336abff9803c34135ecd0eb0a24
nunbitromance <sunghyolee@hotmail.com>
2017-08-05 09:27:56 -0700

    Create FacebookInterview.java

---
+++ src/FacebookInterview.java
@@ -0,0 +1,20 @@
+1. Create an addition function for two string integers.  
+2. Search for an item in a sorted, but rotated, array
+3. Why Facebook and a couple coding questions that I won't disclose
+4. reverse linkedlist, word ladder, wildcard matching 
+5. Given a string and a list of alphabetic letters, find the minimum length of substring that contains all the characters given in O(n) time.
+6. Convert a BST to a circular linked list  
+7. Print out a graph in column order, starting from left to right
+8. Implement a queue using a circular buffer.  
+9. Write a function that takes an integer and prints out the English text of it. ex. Input: 1432; Output: 'One thousand, four hundred thirty-two'  
+10. How to find all anagrams of a word given a dictionary?  
+11. Write a function for testing "endianness."  
+12. Write push/pop functions for a ring buffer. 
+13. Given two words and a dictionary, print the path from one word to the next changing one letter at a time  
+14. Given two identical DOM tree structures, A and B, and a node from A, find the corresponding node in B.  
+15. Implement a sorted circular linked list.
+16. Given an n*n matrix filled randomly with different colors (no limit on what the colors are), add up the total number of groups of each color - a group is adjacent cells of the same color touching each other. Clarifying question: are diagonals adjacent (A: no) 
+17. Write a system to parse byte chunks of messages
+18. Merge a list of (possibly overlapping) time intervals and return a sorted list of non-overlapping time intervals. 
+19. How would you implement a Naive String matching program? 2) How do you know when if a binary tree is a BST?
+20. Given 2 strings that you are reading from 2 streams (so you don't know the length of any of the strings and the only method you have is getNextChar() for each string), implement a program that tells you when 2 of these strings are 1 edit away from being the same. 1- edit is defined as a single insert char, single remove char or single modify/change char in only 1 of the strings.  
