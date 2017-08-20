1257a131186d659421a80003193f394ec2d4e838
nunbitromance <sunghyolee@hotmail.com>
2017-08-20 09:45:54 -0700

    Create IslandPerimeter.java

---
+++ src/IslandPerimeter.java
@@ -0,0 +1,38 @@
+/*
+https://leetcode.com/problems/island-perimeter/description/
+
+You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water. Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells). The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+Example:
+
+[[0,1,0,0],
+ [1,1,1,0],
+ [0,1,0,0],
+ [1,1,0,0]]
+
+Answer: 16
+Explanation: The perimeter is the 16 yellow stripes in the image below:
+
+
+*/
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int c = 0;
+        int n = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                if (grid[i][j] == 1) {
+                    c++;
+                    // up
+                    if (i+1 < grid.length && grid[i+1][j] == 1) {
+                        n++;
+                    } 
+                    if (j + 1 < grid[0].length && grid[i][j+1] == 1) {
+                        n++;
+                    }
+                }
+            }
+        }
+        return c * 4 - n * 2;
+    }
+}
