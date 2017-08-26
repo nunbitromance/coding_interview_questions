afd2fe59079aaed59e7ae907d9e607dc889ad40c
nunbitromance <sunghyolee@hotmail.com>
2017-08-26 09:50:50 -0700

    Update Battleships.java

--- src/Battleships.java
+++ src/Battleships.java
@@ -1,21 +1,18 @@
-Going over all cells, we can count only those that are the "first" cell of the battleship. First cell will be defined as the most top-left cell. We can check for first cells by only counting cells that do not have an 'X' to the left and do not have an 'X' above them.
+Going over all cells, we can count only those that are the "first" cell of the battleship. 
+    First cell will be defined as the most top-left cell. We can check for first cells by only counting cells that do not 
+    have an 'X' to the left and do not have an 'X' above them.
 
 
     public int countBattleships(char[][] board) {
-        int m = board.length;
-        if (m==0) return 0;
-        int n = board[0].length;
-        
-        int count=0;
-        
-        for (int i=0; i<m; i++) {
-            for (int j=0; j<n; j++) {
-                if (board[i][j] == '.') continue;
-                if (i > 0 && board[i-1][j] == 'X') continue;
-                if (j > 0 && board[i][j-1] == 'X') continue;
-                count++;
+        int c = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'X' && 
+                    (i == 0 || board[i - 1][j] == '.') && 
+                    (j == 0 || board[i][j - 1] == '.')) {
+                    c++;
+                }
             }
         }
-        
-        return count;
+        return c;
     }
