4c4161116f23fbacf8efc270f1aa0e215eff0891
nunbitromance <sunghyolee@hotmail.com>
2017-08-27 02:02:31 -0700

    Update IsInterleaving.java

--- src/IsInterleaving.java
+++ src/IsInterleaving.java
@@ -9,91 +9,38 @@ s2 = "dbbca",
 
 When s3 = "aadbbcbcac", return true.
 When s3 = "aadbbbaccc", return false.
-*/
+
 // Recursion
 Dynamic Programming
-The worst case time complexity of recursive solution is O(2n). The above recursive solution certainly has many overlapping subproblems. For example, if wee consider A = “XXX”, B = “XXX” and C = “XXXXXX” and draw recursion tree, there will be many overlapping subproblems.
-Therefore, like other typical Dynamic Programming problems, we can solve it by creating a table and store results of subproblems in bottom up manner. Thanks to Abhinav Ramana for suggesting this method and implementation.
+The worst case time complexity of recursive solution is O(2n). The above recursive solution certainly has many overlapping subproblems. 
+For example, if wee consider A = “XXX”, B = “XXX” and C = “XXXXXX” and draw recursion tree, there will be many overlapping subproblems.
+Therefore, like other typical Dynamic Programming problems, we can solve it by creating a table and store results of subproblems in bottom
+up manner. Thanks to Abhinav Ramana for suggesting this method and implementation.
 
-// A Dynamic Programming based program to check whether a string C is
-// an interleaving of two other strings A and B.
-#include <iostream>
-#include <string.h>
-using namespace std;
- 
-// The main function that returns true if C is
-// an interleaving of A and B, otherwise false.
-bool isInterleaved(char* A, char* B, char* C)
-{
-    // Find lengths of the two strings
-    int M = strlen(A), N = strlen(B);
- 
-    // Let us create a 2D table to store solutions of
-    // subproblems.  C[i][j] will be true if C[0..i+j-1]
-    // is an interleaving of A[0..i-1] and B[0..j-1].
-    bool IL[M+1][N+1];
- 
-    memset(IL, 0, sizeof(IL)); // Initialize all values as false.
- 
-    // C can be an interleaving of A and B only of sum
-    // of lengths of A & B is equal to length of C.
-    if ((M+N) != strlen(C))
-       return false;
- 
-    // Process all characters of A and B
-    for (int i=0; i<=M; ++i)
-    {
-        for (int j=0; j<=N; ++j)
-        {
-            // two empty strings have an empty string
-            // as interleaving
-            if (i==0 && j==0)
-                IL[i][j] = true;
- 
-            // A is empty
-            else if (i==0 && B[j-1]==C[j-1])
-                IL[i][j] = IL[i][j-1];
- 
-            // B is empty
-            else if (j==0 && A[i-1]==C[i-1])
-                IL[i][j] = IL[i-1][j];
- 
-            // Current character of C matches with current character of A,
-            // but doesn't match with current character of B
-            else if(A[i-1]==C[i+j-1] && B[j-1]!=C[i+j-1])
-                IL[i][j] = IL[i-1][j];
- 
-            // Current character of C matches with current character of B,
-            // but doesn't match with current character of A
-            else if (A[i-1]!=C[i+j-1] && B[j-1]==C[i+j-1])
-                IL[i][j] = IL[i][j-1];
- 
-            // Current character of C matches with that of both A and B
-            else if (A[i-1]==C[i+j-1] && B[j-1]==C[i+j-1])
-                IL[i][j]=(IL[i-1][j] || IL[i][j-1]) ;
+*/
+public boolean isInterleave(String s1, String s2, String s3) {
+
+    if ((s1.length()+s2.length())!=s3.length()) return false;
+
+    boolean[][] matrix = new boolean[s2.length()+1][s1.length()+1];
+
+    matrix[0][0] = true;
+
+    for (int i = 1; i < matrix[0].length; i++){
+        matrix[0][i] = matrix[0][i-1]&&(s1.charAt(i-1)==s3.charAt(i-1));
+    }
+
+    for (int i = 1; i < matrix.length; i++){
+        matrix[i][0] = matrix[i-1][0]&&(s2.charAt(i-1)==s3.charAt(i-1));
+    }
+
+    for (int i = 1; i < matrix.length; i++){
+        for (int j = 1; j < matrix[0].length; j++){
+            matrix[i][j] = (matrix[i-1][j]&&(s2.charAt(i-1)==s3.charAt(i+j-1)))
+                    || (matrix[i][j-1]&&(s1.charAt(j-1)==s3.charAt(i+j-1)));
         }
     }
- 
-    return IL[M][N];
-}
- 
-// A function to run test cases
-void test(char *A, char *B, char *C)
-{
-    if (isInterleaved(A, B, C))
-        cout << C <<" is interleaved of " << A <<" and " << B << endl;
-    else
-        cout << C <<" is not interleaved of " << A <<" and " << B << endl;
-}
- 
- 
-// Driver program to test above functions
-int main()
-{
-    test("XXY", "XXZ", "XXZXXXY");
-    test("XY" ,"WZ" ,"WZXY");
-    test ("XY", "X", "XXY");
-    test ("YX", "X", "XXY");
-    test ("XXY", "XXZ", "XXXXZY");
-    return 0;
+
+    return matrix[s2.length()][s1.length()];
+
 }
