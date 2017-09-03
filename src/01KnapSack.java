3922e6d2bd2a587e3138184f09e19c0cf720ccaf
nunbitromance <sunghyolee@hotmail.com>
2017-09-03 15:31:46 -0700

    Update 01KnapSack.java

--- src/01KnapSack.java
+++ src/01KnapSack.java
@@ -1,15 +1,15 @@
     public int calculate(int val[], int wt[], int W){
         int K[][] = new int[val.length+1][W+1];
         
-        for(int i=0; i <= val.length; i++){
-            for(int j=0; j <= W; j++){
+        for(int j=0; j <= W; j++){
+            for(int i=0; i <= val.length; i++){
                 if(i == 0 || j == 0){
                     K[i][j] = 0;
                     continue;
                 }
-                if(j - wt[i-1] >= 0){
+                if(j > wt[i-1]){
                     K[i][j] = Math.max(K[i-1][j], K[i-1][j-wt[i-1]] + val[i-1]);
-                }else{
+                } else{
                     K[i][j] = K[i-1][j];
                 }
             }
