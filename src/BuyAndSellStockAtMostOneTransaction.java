62c17a0b8752386496120537b879d89ff746108c
nunbitromance <sunghyolee@hotmail.com>
2017-09-03 14:42:45 -0700

    Update BuyAndSellStockAtMostOneTransaction.java

--- src/BuyAndSellStockAtMostOneTransaction.java
+++ src/BuyAndSellStockAtMostOneTransaction.java
@@ -4,7 +4,7 @@ Say you have an array for which the ith element is the price of a given stock on
 
 If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 */
-public int maxProfix(int[] prices) {
+public int maxProfit(int[] prices) {
   int minIndex = 0;
   int maxDiff = 0;
   int curDiff = 0;
